88e4d1e90d610efd786ac100cf17ab21446e3a16
Guilherme Balog Gardino <guilherme.gardino@usp.br>
2021-10-09 20:25:08 -0300

    feat: split sentence in half if its too long

--- src/Presenter.ts
+++ src/Presenter.ts
@@ -4,6 +4,7 @@ export class Presenter {
   private slideMaker: SlideMaker
   private readonly VERSE_SEPARATOR = '\n\n'
   private readonly SENTENCE_SEPARATOR = '\n'
+  private readonly WORD_SEPARATOR = ' '
 
   constructor(slideMaker: SlideMaker) {
     this.slideMaker = slideMaker
@@ -17,36 +18,48 @@ export class Presenter {
     while(i < verses.length) {
       const concat = [...versesToBeWritten, verses[i]]
 
-      if(this.slideMaker.doesTextFit(concat.join(this.VERSE_SEPARATOR))){
+      if(this.slideMaker.doesTextFit(concat.join(this.VERSE_SEPARATOR))) {
         versesToBeWritten.push(verses[i])
         i++
-      } else {
-        if(versesToBeWritten.length == 0) {
-          verses = verses.flatMap((verse, j) => {
-            return i == j ? this.splitVerseInHalf(verses[i]) : verse
-          })
-
-          continue
-        }
-
-        this.slideMaker.writeText(versesToBeWritten.join(this.VERSE_SEPARATOR))
-        versesToBeWritten = []
+        continue
       }
+
+      if(versesToBeWritten.length == 0) {
+        verses = verses.flatMap((verse, j) => {
+          return i == j ? this.splitVerseInHalf(verses[i]) : verse
+        })
+
+        continue
+      }
+
+      this.slideMaker.writeText(versesToBeWritten.join(this.VERSE_SEPARATOR))
+      versesToBeWritten = []
     }
 
     this.slideMaker.writeText(versesToBeWritten.join(this.VERSE_SEPARATOR))
   }
 
   private splitLyricInVerses(lyrics: string) {
-    return lyrics.split(this.VERSE_SEPARATOR)
+    return lyrics
+      .split(this.VERSE_SEPARATOR)
+      .map(verse => verse.trim())
+      .filter(verse => verse.length > 0)
   }
 
   private splitVerseInHalf(verse: string) {
-    const sentences = verse.split(this.SENTENCE_SEPARATOR)
-    const middle = Math.round(sentences.length / 2)
+    if(verse.includes(this.SENTENCE_SEPARATOR)){
+      return this.splitInHalf(verse, this.SENTENCE_SEPARATOR)
+    } else {
+      return this.splitInHalf(verse, this.WORD_SEPARATOR)
+    }
+  }
 
-    const head = sentences.slice(0, middle).join(this.SENTENCE_SEPARATOR)
-    const tail = sentences.slice(middle, sentences.length).join(this.SENTENCE_SEPARATOR)
+  private splitInHalf(text: string, separator: string) {
+    const parts = text.split(separator)
+    const middle = Math.round(parts.length / 2)
+
+    const head = parts.slice(0, middle).join(separator)
+    const tail = parts.slice(middle, parts.length).join(separator)
 
     return [ head, tail ]
   }
