243972f3648e22caa7443dd71ecda55764626379
Guilherme Balog Gardino <guilherme.gardino@usp.br>
2021-10-09 16:47:44 -0300

    fix: split verse in half if its too long

--- src/Presenter.ts
+++ src/Presenter.ts
@@ -3,23 +3,34 @@ import { SlideMaker } from "./SlideMaker"
 export class Presenter {
   private slideMaker: SlideMaker
   private readonly VERSE_SEPARATOR = '\n\n'
+  private readonly SENTENCE_SEPARATOR = '\n'
 
   constructor(slideMaker: SlideMaker) {
     this.slideMaker = slideMaker
   }
 
   public writeLyrics(lyrics: string) {
-    const verses = this.splitLyricInVerses(lyrics)
+    let verses = this.splitLyricInVerses(lyrics)
     let versesToBeWritten: string[] = []
 
-    for(let i = 0; i < verses.length; i++) {
+    let i = 0
+    while(i < verses.length) {
       const concat = [...versesToBeWritten, verses[i]]
 
       if(this.slideMaker.doesTextFit(concat.join(this.VERSE_SEPARATOR))){
         versesToBeWritten.push(verses[i])
+        i++
       } else {
+        if(versesToBeWritten.length == 0) {
+          verses = verses.flatMap((verse, j) => {
+            return i == j ? this.splitVerseInHalf(verses[i]) : verse
+          })
+
+          continue
+        }
+
         this.slideMaker.writeText(versesToBeWritten.join(this.VERSE_SEPARATOR))
-        versesToBeWritten = [verses[i]]
+        versesToBeWritten = []
       }
     }
 
@@ -30,4 +41,13 @@ export class Presenter {
     return lyrics.split(this.VERSE_SEPARATOR)
   }
 
+  private splitVerseInHalf(verse: string) {
+    const sentences = verse.split(this.SENTENCE_SEPARATOR)
+    const middle = Math.round(sentences.length / 2)
+
+    const head = sentences.slice(0, middle).join(this.SENTENCE_SEPARATOR)
+    const tail = sentences.slice(middle, sentences.length).join(this.SENTENCE_SEPARATOR)
+
+    return [ head, tail ]
+  }
 }
